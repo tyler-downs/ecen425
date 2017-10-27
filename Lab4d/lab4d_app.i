667115cc7858eef449e661b7b8d08c67247c390e
Tyler William Downs <tylerwd@ssh3.et.byu.edu>
2017-10-26 19:53:12 -0600

    Set up workspace for Lab4d, began work. Code works with ticks at 10000 but not at 750

---
+++ Lab4d/lab4d_app.i
@@ -0,0 +1,209 @@
+# 1 "lab4d_app.c"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 1 "<command-line>" 2
+# 1 "lab4d_app.c"
+
+
+
+
+
+
+# 1 "clib.h" 1
+
+
+
+void print(char *string, int length);
+void printNewLine(void);
+void printChar(char c);
+void printString(char *string);
+
+
+void printInt(int val);
+void printLong(long val);
+void printUInt(unsigned val);
+void printULong(unsigned long val);
+
+
+void printByte(char val);
+void printWord(int val);
+void printDWord(long val);
+
+
+void exit(unsigned char code);
+
+
+void signalEOI(void);
+# 8 "lab4d_app.c" 2
+# 1 "yakk.h" 1
+# 1 "linkedList.h" 1
+# 1 "yaku.h" 1
+# 2 "linkedList.h" 2
+
+
+
+
+
+
+struct context_type
+{
+ unsigned sp;
+ unsigned int ip;
+
+ unsigned int ax;
+ unsigned int bx;
+ unsigned int cx;
+ unsigned int dx;
+ unsigned int si;
+ unsigned int di;
+ unsigned int bp;
+ unsigned int es;
+ unsigned int ds;
+ unsigned int cs;
+ unsigned int flags;
+};
+
+typedef struct taskblock *TCBptr;
+
+typedef struct taskblock
+{
+ struct context_type context;
+ void *stackptr;
+ unsigned priority;
+ int delay;
+ TCBptr next;
+ TCBptr prev;
+ unsigned ID;
+} TCB;
+
+extern TCBptr YKRdyList;
+extern TCBptr YKSuspList;
+
+TCBptr createTCB(void *stackptr, int priority, struct context_type context);
+
+void insertTCBIntoRdyList(TCBptr tcb);
+
+void removeFirstTCBFromRdyList();
+
+
+
+
+
+void moveTCBToRdyList(TCBptr tmp);
+
+
+
+void printTCBs();
+void printLists();
+# 2 "yakk.h" 2
+
+
+
+
+
+extern unsigned int YKCtxSwCount;
+extern unsigned int YKIdleCount;
+extern unsigned int YKTickNum;
+extern unsigned int running_flag;
+extern TCBptr lastRunningTask;
+
+void YKIdleTask();
+static int IdleStk[256];
+
+
+void YKInitialize(void);
+
+void YKEnterMutex(void);
+
+void YKExitMutex(void);
+
+void YKIdleTask(void);
+
+void YKNewTask(void (* task)(void), void *taskStack, unsigned char priority);
+
+void YKRun(void);
+
+void YKDelayTask(unsigned count);
+
+void YKEnterISR(void);
+
+void YKExitISR(void);
+
+void YKScheduler(void);
+
+void YKDispatcher(void);
+
+void YKFirstDispatcher(void);
+
+void YKTickHandler(void);
+# 9 "lab4d_app.c" 2
+
+
+
+
+
+
+int AStk[256];
+int BStk[256];
+int CStk[256];
+int DStk[256];
+
+void ATask(void);
+void BTask(void);
+void CTask(void);
+void DTask(void);
+
+void main(void)
+{
+    YKInitialize();
+
+    printString("Creating tasks...\n");
+    YKNewTask(ATask, (void *) &AStk[256], 3);
+    YKNewTask(BTask, (void *) &BStk[256], 5);
+    YKNewTask(CTask, (void *) &CStk[256], 7);
+    YKNewTask(DTask, (void *) &DStk[256], 8);
+
+    printString("Starting kernel...\n");
+    YKRun();
+}
+
+void ATask(void)
+{
+    printString("Task A started.\n");
+    while (1)
+    {
+        printString("Task A, delaying 2.\n");
+        YKDelayTask(2);
+    }
+}
+
+void BTask(void)
+{
+    printString("Task B started.\n");
+    while (1)
+    {
+        printString("Task B, delaying 3.\n");
+        YKDelayTask(3);
+    }
+}
+
+void CTask(void)
+{
+    printString("Task C started.\n");
+    while (1)
+    {
+        printString("Task C, delaying 5.\n");
+        YKDelayTask(5);
+    }
+}
+
+void DTask(void)
+{
+    printString("Task D started.\n");
+    while (1)
+    {
+        printString("Task D, delaying 10.\n");
+        YKDelayTask(10);
+    }
+}
---
+++ Lab4d/yakc.c
@@ -0,0 +1,190 @@
+#include "yakk.h"
+#include "clib.h"
+//#include "linkedList.h"
+
+unsigned int YKIdleCount = 0;
+unsigned int running_flag = 0; //0 means not running, 1 means running
+unsigned int ISRCallDepth = 0;
+TCBptr lastRunTask = NULL;
+unsigned int YKCtxSwCount = 0;
+unsigned int YKTickNum = 0;
+unsigned int firstTime = 1;
+
+
+//ARE THESE THE VALUES WE WANT?
+struct context_type initContext = {
+	0, //sp //this should be the bottom of the task's stack
+	0, //ip //this should be the function pointer value
+	//	ready, //ready
+	0, //ax
+	0, //bx
+	0, //cx
+	0, //dx
+	0, //si
+	0, //di
+	0, //bp //this should also be the stack
+	0, //es
+	0, //ds
+	0, //cs
+	0x0200 //flags
+};
+
+void YKInitialize(void)
+{
+		//DO I NEED TO TURN OFF INTERRUPTS?
+	//create YKIdleTask
+		//allocate stack space
+	YKNewTask(YKIdleTask, (void *) &IdleStk[IDLE_TASK_STACK_SIZE], LOWEST_PRIORITY);
+	//set lastRunTask to idle task (the only thing on ready list)
+	lastRunTask = YKRdyList;
+}
+
+void YKIdleTask(void)
+{
+	printString("Entering idle task\n\r");
+	while(1)
+	{
+		int tmp;
+		tmp = YKIdleCount + 1;
+		YKIdleCount = tmp;
+	//	printString("YKIdleCount = ");
+	//	printInt(YKIdleCount);
+	//	printString("\n");
+	}
+	//should take 4 instructions. //NEED TO VERIFY
+}
+
+
+void YKNewTask(void (* task)(void), void *taskStack, unsigned char priority)
+{
+	//register task
+		//create TCB
+		//init TCB values, including registers, interrupt flag should be 1
+		//add to linked list
+	//if YKRun has been called, call the scheduler
+
+	TCBptr newTCB;
+	newTCB = createTCB(taskStack, priority, initContext);
+	//now change the context parts that need changing
+	newTCB->context.sp = (int)(taskStack);
+	newTCB->context.ip = (int)task;
+	newTCB->context.bp = (int)taskStack;
+	insertTCBIntoRdyList(newTCB); //do we start all tasks as ready?? //TODO
+	//printString("Starting new task.\n\r");
+	//printTCBs(); //test
+	//printLists(); //test
+	if (running_flag)
+	{
+		YKScheduler();
+	}
+}
+
+void YKRun(void)
+{
+	//set the running flag
+	//run scheduler
+	running_flag = 1;
+	//printString("Running!!\n");
+	//ENABLE INTERRUPTS?
+	YKScheduler();
+}
+
+void YKDelayTask(unsigned count)
+{
+	//if count == 0, return
+	//set task state to blocked
+	//set global variable to count
+	//call scheduler
+	if (count == 0)
+		return;
+
+
+	//grab the running task at the top of the ready list
+	//YKRdyList.state = suspended;
+	YKRdyList->delay = count;
+	YKEnterMutex();
+	removeFirstTCBFromRdyList(); //move to suspended list
+	YKExitMutex();
+
+	//print the ready list (for debug)
+	//printLists();
+
+	YKScheduler();
+}
+
+void YKEnterISR(void)
+{
+	ISRCallDepth++;
+}
+
+void YKExitISR(void)
+{
+	ISRCallDepth--;
+	if (ISRCallDepth == 0)
+		YKScheduler();
+}
+
+void YKScheduler(void)
+{
+	//determine highest priority task
+	//if highest priority task != lastRunTask
+		//call dispatcher
+	//printString("Entering scheduler\n\r");
+	if (firstTime)
+	{
+		firstTime = 0;
+		//printString("Calling the dispatcher\n");
+		YKCtxSwCount++;
+		//printLists();
+		YKFirstDispatcher(); //call the first dispatcher.
+	}
+	else if (lastRunTask != YKRdyList) //if the task has changed
+	{
+		//printString("Calling the dispatcher\n");
+		YKCtxSwCount++;
+		//printLists();
+		YKDispatcher(); //call the dispatcher.
+	}
+
+}
+
+void YKTickHandler(void)
+{
+	//decrement all tick count globals
+	//for all that are 0
+		//set that task's state to ready in its TCB
+	//++YKTickNum
+	//call user tick handler if exists
+
+	TCBptr tmp;
+	TCBptr tmp2;
+	tmp  = YKSuspList;
+	//printString("tmp = YKSusplist: ");
+	//printInt((int)tmp);
+	//printString("\ntmp->delay: ");
+	//printInt(tmp->delay);
+	printString("\n");
+
+	 while(tmp != NULL)
+	 {
+			if (tmp->delay > 0)
+			{
+				(tmp->delay)--;
+				if (tmp->delay == 0)
+				{
+					tmp2 = tmp;
+					tmp = tmp->next;
+					//printString("Moved TCB to ready list\n\r");
+					moveTCBToRdyList(tmp2); //moving lists changes state
+				}
+				else
+					tmp = tmp->next;
+			}
+			else
+				tmp = tmp->next;
+	 }
+	YKTickNum++;
+	//call user tick handler if exists
+	//call the scheduler
+	YKScheduler();
+}
